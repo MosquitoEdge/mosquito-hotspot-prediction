432124f0d4c92ccd9acc0e52e1fbc3ddc4a20b18
Govind Gnanakumar <govind.gnanakumar@outlook.com>
2021-07-09 21:50:11 -0700

    Continue sentinel 3 experimentations

--- sentinel/temperature.py
+++ sentinel/temperature.py
@@ -7,33 +7,31 @@ now = datetime.now().isoformat(timespec='seconds') + "Z"
 
 def gen_temp(bbox: Tuple[float], from_date: str = "2021-04-01T00:00:00Z", to_date: str = now) -> Dict:
     evalscript = """
-            //VERSION=3
-            function setup() {
-            return {
-                input: [{
-                bands: [
-                    "CLM",
-                    "dataMask"
-                ]
-                }],
-                output: [
-                {
-                    id: "data",
-                    bands: 1
-                },
-                {
-                    id: "dataMask",
-                    bands: 1
-                }]
-            }
-            }
-            function evaluatePixel(samples) {
-                return {
-                    data: [samples.CLM],
-                    dataMask: [samples.dataMask]
-                }
-            }
-            """
+    //VERSION=3
+  function setup() {
+  return {
+    input: [{
+      bands: ["S7", "S8", "S9", "F1", "F2"] 
+    }],
+    output: {
+      bands: 5,
+      sampleType: "UINT16"
+    }
+  }
+}
+
+function multiplyband(sample){
+  // Multiply by 100
+  return 100 * sample;
+}
+
+function evaluatePixel(sample) {
+  // Return the bands multiplied by 100 as integers to save processing units. 
+  // To obtain reflectance or BT values, simply divide the resulting pixel values by 100.
+  return [multiplyband(sample.S7), multiplyband(sample.S8), multiplyband(sample.S9), 
+          multiplyband(sample.F1), multiplyband(sample.F2)]
+}
+    """
     return {
         "input": {
             "bounds": {
@@ -47,7 +45,8 @@ def gen_temp(bbox: Tuple[float], from_date: str = "2021-04-01T00:00:00Z", to_dat
                     "type": "sentinel-3-slstr",
                     "dataFilter": {
                         "mosaickingOrder": "leastRecent"
-                    }
+                    },
+                    "orbitDirection": "DESCENDING"
                 }
             ]
         },
@@ -60,8 +59,8 @@ def gen_temp(bbox: Tuple[float], from_date: str = "2021-04-01T00:00:00Z", to_dat
                 "of": "P1D"
             },
             "evalscript": evalscript,
-            "resx": 10,
-            "resy": 10
+            "resx": 500,
+            "resy": 500
         },
     }
 
