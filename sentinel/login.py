c21d8a92f21b1871b0252fba78b8b9a967e79136
Govind Gnanakumar <47906753+sandkoan@users.noreply.github.com>
2021-07-11 09:12:35 -0700

    Update login.py

--- sentinel/login.py
+++ sentinel/login.py
@@ -1,8 +1,5 @@
 # Your client credentials
-# from getpass import getpass
+from getpass import getpass
 
-# client_id = getpass('Enter the client_id: ')
-# client_secret = getpass('Enter the client_secret: ')
-
-client_id = 'dd929c11-7ba5-46f4-9473-e63b1f5aaf1f'
-client_secret = 'K5y_,KG^0&^ff_7@}YfMSlQVEuNQ|@w9R#tL^!SC'
+client_id = getpass('Enter the client_id: ')
+client_secret = getpass('Enter the client_secret: ')
