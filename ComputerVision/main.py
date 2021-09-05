92f73954582483e2ab05075b3f2d8da566c8e446
rohanavilella <62043083+rohanavilella@users.noreply.github.com>
2021-09-05 00:54:38 -0400

    Update main.py

--- ComputerVision/main.py
+++ ComputerVision/main.py
@@ -1,8 +1,3 @@
-##Contador de personas
-##Federico Mejia
-##from picamera.array import PiRGBArray
-##from picamera import PiCamera
-
 import numpy as np
 import cv2 as cv
 import Person
@@ -60,7 +55,6 @@ try:
 except:
     print("Could not open")
 
-# Contadores de entrada y salida
 cnt_up = 0
 cnt_down = 0
 
@@ -77,7 +71,6 @@ frameArea = h * w
 areaTH = frameArea / 250
 print('Area Threshold', areaTH)
 
-# Lineas de entrada/salida
 line_up = int(2 * (h / 5))
 line_down = int(3 * (h / 5))
 
@@ -106,37 +99,28 @@ pt8 = [w, down_limit];
 pts_L4 = np.array([pt7, pt8], np.int32)
 pts_L4 = pts_L4.reshape((-1, 1, 2))
 
-# Substractor de fondo
 fgbg = cv.createBackgroundSubtractorMOG2(detectShadows=True)
 
-# Elementos estructurantes para filtros morfoogicos
 kernelOp = np.ones((3, 3), np.uint8)
 kernelOp2 = np.ones((5, 5), np.uint8)
 kernelCl = np.ones((11, 11), np.uint8)
 
-# Variables
 font = cv.FONT_HERSHEY_SIMPLEX
 persons = []
 max_p_age = 5
 pid = 1
 
 while (cap.isOpened()):
-    ##for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # Lee una imagen de la fuente de video
+   
     ret, frame = cap.read()
-    ##    frame = image.array
 
     for i in persons:
         i.age_one()  # age every person one frame
-    #########################
-    #   PRE-PROCESAMIENTO   #
-    #########################
+   
 
-    # Aplica substraccion de fondo
     fgmask = fgbg.apply(frame)
     fgmask2 = fgbg.apply(frame)
 
-    # Binariazcion para eliminar sombras (color gris)
     try:
         ret, imBin = cv.threshold(fgmask, 200, 255, cv.THRESH_BINARY)
         ret, imBin2 = cv.threshold(fgmask2, 200, 255, cv.THRESH_BINARY)
@@ -151,21 +135,13 @@ while (cap.isOpened()):
         print('UP:', cnt_up)
         print('DOWN:', cnt_down)
         break
-    #################
-    #   CONTORNOS   #
-    #################
+  
 
-    # RETR_EXTERNAL returns only extreme outer flags. All child contours are left behind.
     contours0, hierarchy = cv.findContours(mask2, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
     for cnt in contours0:
         area = cv.contourArea(cnt)
         if area > areaTH:
-            #################
-            #   TRACKING    #
-            #################
-
-            # Falta agregar condiciones para multipersonas, salidas y entradas de pantalla.
-
+           
             M = cv.moments(cnt)
             cx = int(M['m10'] / M['m00'])
             cy = int(M['m01'] / M['m00'])
@@ -175,7 +151,6 @@ while (cap.isOpened()):
             if cy in range(up_limit, down_limit):
                 for i in persons:
                     if abs(x - i.getX()) <= w and abs(y - i.getY()) <= h:
-                        # el objeto esta cerca de uno que ya se detecto antes
                         new = False
                         i.updateCoords(cx, cy)  # actualiza coordenadas en el objeto and resets age
 
@@ -191,9 +166,7 @@ while (cap.isOpened()):
                             print ("Counter: "+str (counter))
 
 
-                            #if i.going_UP(line_down, line_up) == True:
-                                #print("Taking screenshot... ")
-                                #cv.imwrite("pic2.jpg", img)
+                        
 
                         elif i.going_DOWN(line_down, line_up) == True:
                             cnt_down += 1;
@@ -206,9 +179,7 @@ while (cap.isOpened()):
                             print("Counter: " + str(counter))
 
 
-                                #if i.going_DOWN(line_down, line_up) == True:
-                                    #print("Taking screenshot... ")
-                                    #cv.imwrite("pic3.jpg", img)
+                              
                         break
                     if i.getState() == '1':
                         if i.getDir() == 'down' and i.getY() > down_limit:
@@ -224,9 +195,7 @@ while (cap.isOpened()):
                     p = Person.MyPerson(pid, cx, cy, max_p_age)
                     persons.append(p)
                     pid += 1
-                    #################
-            #   DIBUJOS     #
-            #################
+          
             cv.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
             img = cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
@@ -240,13 +209,6 @@ while (cap.isOpened()):
                 print ("Number of larvae: "+str (len (persons)))
 
 
-            # cv.drawContours(frame, cnt, -1, (0,255,0), 3)
-
-    # END for cnt in contours0
-
-    #########################
-    # DIBUJAR TRAYECTORIAS  #
-    #########################
     for i in persons:
         ##        if len(i.getTracks()) >= 2:
         ##            pts = np.array(i.getTracks(), np.int32)
@@ -256,9 +218,7 @@ while (cap.isOpened()):
         ##            print str(i.getX()), ',', str(i.getY())
         cv.putText(frame, str(i.getId()), (i.getX(), i.getY()), font, 0.3, i.getRGB(), 1, cv.LINE_AA)
 
-    #################
-    #   IMAGANES    #
-    #################
+
     str_up = 'UP: ' + str(cnt_up)
     str_down = 'DOWN: ' + str(cnt_down)
     frame = cv.polylines(frame, [pts_L1], False, line_down_color, thickness=2)
@@ -273,20 +233,14 @@ while (cap.isOpened()):
     cv.imshow('Frame', frame)
     cv.imshow('Mask', mask)
 
-    ##    rawCapture.truncate(0)
-    # preisonar ESC para salir
+  
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
-# END while(cap.isOpened())
 
-#################
-#   LIMPIEZA    #
-#################
 log.flush()
 log.close()
 cap.release()
 cv.destroyAllWindows()
 
-#1. Validate
-#2. Add presence or absence point to training database in MySQL
+
